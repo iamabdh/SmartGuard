6b4110ffc3bc3b8f5e53955d2992440e9b082200
iamabdh <62632931+iamabdh@users.noreply.github.com>
2021-01-04 22:39:14 +0400

    Add files via upload

---
+++ SmartGuard/main.py
@@ -0,0 +1,29 @@
+from firebase import firebase
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
+
+
+
+class SensorData (Label):
+    def update (self, *args):
+        status = firebase.FirebaseApplication('https://esp-32-d8e67-default-rtdb.firebaseio.com/', None).get('/DATA', None)
+        self.font_size = ('30sp')
+        if status == 1:
+            self.text = "Thief Spotted!!"
+            self.color= 1, 0, 0, 1
+            sound = SoundLoader.load('beep.wav').play()
+        else :
+            self.color = 1, 1, 1, 1
+            self.text = "EveryThing Look Fine So Far .."
+
+class MyApp (App):
+    def build(self):
+        latestUpdate = SensorData()
+        Clock.schedule_interval(latestUpdate.update, 1) #replace here
+        return latestUpdate
+
+if __name__ == '__main__':
+    app = MyApp()
+    app.run()
